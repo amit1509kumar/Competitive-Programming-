510c3cadfbfb84a131fbcb971566bf7ed502669f
Amit Kumar <amit1509kumar@gmail.com>
2019-08-06 01:49:02 +0530

    Problem B. Zero Array

---
+++ CodeForces/Round-577/ProblemB.cs
@@ -0,0 +1,30 @@
+﻿//problem link -https://codeforces.com/contest/1201/problem/B
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Competitive_Programming.CodeForces.Round_577
+{
+    public class ProblemB
+    {
+        public void Solve()
+        {
+            int n = Reader.ReadInt();
+            int[] A = Reader.ReadArray();
+
+            long sum = 0;
+            int max = 0;
+
+            foreach (var a in A)
+            {
+                sum += a;
+                max = Math.Max(a, max);
+            }
+
+
+
+            Console.WriteLine(sum % 2 == 0 && sum - max >= max ? "YES" : "NO");
+        }
+    }
+}
